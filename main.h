9b65219f0436c61b77291d8e20a0b4f157c42d74
Sebastian-Daniel Staretu <125968790+daniel-staretu@users.noreply.github.com>
2025-12-19 11:59:16 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+Mat openImage(string path);
+Mat closingMorphological(Mat input_image);
+Mat toGrayscale(Mat input_image);
+Mat thresholdingOperation(Mat input_image);
+Mat edgeDetection(Mat input_image);
+Mat cornerDetection(Mat input_image, vector<Point>& sorted_corners);
+Mat perspectiveTransform(Mat input_image, const vector<Point>& corners);
+
+#endif
